83feb236a7ec4329a6bb1d4f33fd1efc7b41ab5d
YFKxenigsegg <matveykaklanter@mail.ru>
2021-01-31 01:35:28 +0300

    add status field and transfer message to base exception class in constructor

--- ShortLinks.Models/Exceptions/IncorrectDataException.cs
+++ ShortLinks.Models/Exceptions/IncorrectDataException.cs
@@ -1,12 +1,14 @@
 ﻿using System;
+using System.Net;
 
 namespace ShortLinks.Models.Exceptions
 {
     public class IncorrectDataException : Exception
     {
-        public IncorrectDataException(string mes)
+        public HttpStatusCode Status { get; private set; }
+        public IncorrectDataException(HttpStatusCode status, string msg) : base(msg)
         {
-
+            Status = status;
         }
 
     }
